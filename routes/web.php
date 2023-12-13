bc7fcdd606333289e7168f250cda30a8ac7c70c7
DavideCavallotto <davidecavallotto92@gmail.com>
2023-12-13 14:37:17 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Guest\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])->name('index');
